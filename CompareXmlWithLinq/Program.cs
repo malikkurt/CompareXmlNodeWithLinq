80f4152e00237dd3b5c9b198417727ecf8031a2f
Malik Kurt <malikkurt612@gmail.com>
2023-08-11 16:22:15 +0300

    Elements Listing ı guess

--- CompareXmlWithLinq/Program.cs
+++ CompareXmlWithLinq/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.Xml.Linq;
+﻿
+using System;
+using System.Runtime.Serialization.Json;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Program
 {
@@ -6,89 +10,83 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            
 
             XDocument xDocumentLive = XDocument.Load("GetLiveSportsLive.xml");
             XDocument xDocumentTest = XDocument.Load("GetLiveSportsTest.xml");
 
-           
-            Console.WriteLine("-------------------------------------------------------------------");
+
+
+
+
+            IEnumerable<XElement> childlist = from el in xDocumentLive.Root.Elements().Elements()
+                                              select el;
             
-            IEnumerable<XElement> childlist = from el in xDocumentLive.Root.Elements().Elements().Elements()
+            
+            IEnumerable<XElement> childlist2 = from el in xDocumentTest.Root.Elements().Elements()
                                               select el;
 
 
-            //foreach (XElement childlive in childlist)
-            //{
-            //    Console.WriteLine(childlive);
-            //    Console.WriteLine("------------");
-            //}
 
-            //Console.WriteLine("------------");
-
-            IEnumerable<XElement> childlist2 = from el in xDocumentTest.Root.Elements().Elements().Elements()
-                                               select el;
-
+            foreach (XElement childlive in childlist)
+            {
+                Console.WriteLine(childlive);
+                Console.WriteLine("------------");
+            }
 
 
             //foreach (XElement childTest in childlist2)
             //{
             //    Console.WriteLine(childTest);
+            //    Console.WriteLine("------------");
             //}
 
-            //Console.WriteLine("------------");
+
+            var liveElements = xDocumentLive.Descendants("xs:element");
 
 
-            foreach (XElement child in childlist)
-            {
-                bool result = childlist2.Contains(child);
+            //var missingElements = liveElements.Where(liveElem => !testElements.Any(testElem => testElem.Attribute("name").Value == liveElem.Attribute("name").Value));
+            //foreach (var missingElement in missingElements)
+            //{
+            //    differences.Add("Element " + missingElement.Attribute("name").Value + " is missing in test WSDL.");
+            //}
 
-                if (result = true)
-                {
+            //// Compare sequence differences
+            //foreach (var liveElement in liveElements)
+            //{
+            //    string elementName = liveElement.Attribute("name").Value;
+            //    var testElement = testElements.FirstOrDefault(testElem => testElem.Attribute("name").Value == elementName);
 
-                }
-                else if(result =)
-                {
-                    Console.WriteLine("----" + result + "----");
-                    Console.WriteLine(child);
-                }
+            //    if (testElement != null)
+            //    {
+            //        var liveSequence = liveElement.Descendants("xs:sequence").FirstOrDefault();
+            //        var testSequence = testElement.Descendants("xs:sequence").FirstOrDefault();
+
+            //        if (liveSequence != null && testSequence != null)
+            //        {
+            //            bool sequenceMatch = XNode.DeepEquals(liveSequence, testSequence);
+            //            if (!sequenceMatch)
+            //            {
+            //                differences.Add("Element " + elementName + " sequence is different between live and test WSDL.");
+            //            }
+            //        }
+            //    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             }
 
-
-
-
-
-
-
-            //foreach (XElement el in difference)
-            //{
-            //    Console.WriteLine(el.ToString());
-            //}
-
-
-
-
-            //foreach(XElement nodeLoop in childlist)
-            //{
-            //    foreach(XElement nodeLoop_2 in childlist2)
-            //    {
-            //        var differenceNode = 
-            //            from el in nodeLoop_2.Elements() ==
-            //    }
-            //}
-
-
-
-
-        }
-
-        
-
-
-       
     }
 
-        
 }
-
