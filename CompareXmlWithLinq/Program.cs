1adc3af049086006c7f281080b6d6e088f07ec75
Malik Kurt <malikkurt612@gmail.com>
2023-08-15 16:29:55 +0300

    Seraching complexType Elements

--- CompareXmlWithLinq/Program.cs
+++ CompareXmlWithLinq/Program.cs
@@ -18,64 +18,16 @@ namespace Program
             XDocument xDocumentLive = XDocument.Load("GetLiveSportsLive.xml");
             XDocument xDocumentTest = XDocument.Load("GetLiveSportsTest.xml");
 
-            string resultOutputILiveSportsBS = null;
+            
+
+            Console.WriteLine("Enter Node Road:");
 
 
-
-
-            //IEnumerable<XElement> childlist = from el in xDocumentLive.Root.Element()
-            //                                  select el;
-
-
-            //IEnumerable<XElement> childlist2 = from el in xDocumentTest.Root.Elements().Elements()
-            //                                   select el;
-
-
-
-            //foreach (XElement childlive in childlist)
-            //{
-            //    Console.WriteLine(childlive);
-            //    Console.WriteLine("------------");
-            //}
-
-
-            //foreach (XElement childTest in childlist2)
-            //{
-            //    Console.WriteLine(childTest);
-            //    Console.WriteLine("------------");
-            //}
-
-
-            //foreach (XElement child in childGetLive)
-            //{
-            //    Console.WriteLine(child);
-            //}
-
-            string choosenNode = "GetLiveSports";
-
+            string choosenNode = Console.ReadLine();
 
             XNamespace wsdl = "http://schemas.xmlsoap.org/wsdl/";
 
-            Console.WriteLine("--------output--------");
-
-            foreach (XElement xElementLive in xDocumentLive.Root.Descendants().Where(n => n.Name == wsdl + "output"))
-            {
-
-                XElement xElement = XElement.Parse(xElementLive.ToString());
-
-                string result = xElement.FirstAttribute.ToString();
-
-                string lastResult = result.Substring(result.LastIndexOf("/") + 1);
-
-                string lastlastResult = lastResult.Substring(0, lastResult.Length - 1);
-
-                Console.WriteLine(lastlastResult);
-
-            }
-
-
-            
-
+            XNamespace xs = "http://www.w3.org/2001/XMLSchema";
 
 
 
@@ -94,49 +46,122 @@ namespace Program
 
             //}
 
+            Console.WriteLine("--------");
+
             foreach (XElement xElementLive in xDocumentLive.Root.Descendants().Where(n => n.Name == wsdl + "portType"))
             {
-                foreach(XElement xElement in xElementLive.Descendants(wsdl + "operation")){
-                    
-                    XElement xElementParse = XElement.Parse(xElement.ToString());
+                foreach (XElement xElement in xElementLive.Descendants(wsdl + "operation"))
+                {
 
-                    string xElementToString = xElementParse.FirstAttribute.ToString();
+                    string xElementToString = xElement.FirstAttribute.ToString();
 
                     string xElementToSubstring = xElementToString.Substring(xElementToString.LastIndexOf("=") + 1);
 
                     string xElementToSubstringDouble = xElementToSubstring.Substring(1, xElementToSubstring.Length - 2);
 
-
                     if (xElementToSubstringDouble == choosenNode)
                     {
                         Console.WriteLine("Buldum " + xElementToSubstring);
 
-                        choosenNode = xElementToSubstringDouble;
+                        XElement xElementOutput = xElement.Element(wsdl + "output");
 
-                        XElement xElementOutput = xElementParse.Element(wsdl + "output");
+                        XElement xElementOutputParse = XElement.Parse(xElementOutput.ToString());
 
-                        XElement xElement1 = XElement.Parse(xElementOutput.ToString());
-
-                        string resultOutput = xElement1.FirstAttribute.ToString();
+                        string resultOutput = xElementOutputParse.FirstAttribute.ToString();
 
                         string lastResultOutput = resultOutput.Substring(resultOutput.LastIndexOf("/"));
 
                         string lastlastResultOutput = lastResultOutput.Substring(1, lastResultOutput.Length - 2);
 
-                        resultOutputILiveSportsBS = lastlastResultOutput;
+                        choosenNode = lastlastResultOutput;
 
                         Console.WriteLine(lastlastResultOutput);
 
                         break;
-                     
-                        
-
-
-                    }                    
+                    }
                 }
             }
-            
-        }
-    }
 
+
+            Console.WriteLine("--------");
+
+
+            foreach (XElement xElementLive in xDocumentLive.Root.Descendants().Where(n => n.Name == wsdl + "types"))
+            {
+                foreach (XElement xElement1 in xElementLive.Descendants(xs + "element"))
+                {
+
+                    string xElementToString = xElement1.FirstAttribute.ToString();
+
+                    if (xElementToString.Contains("minOccurs"))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        string xElementToSubstring = xElementToString.Substring(xElementToString.LastIndexOf("=") + 1);
+
+                        string xElementToSubstringDouble = xElementToSubstring.Substring(1, xElementToSubstring.Length - 2);
+
+                        Console.WriteLine(xElementToSubstringDouble);
+
+                        if (xElementToSubstringDouble == choosenNode)
+                        {
+                            foreach (XElement xElement in xElement1.Descendants(xs + "element"))
+                            {
+                                string xElementString = xElement.Attribute("type").Value;
+
+                                string xElementStringToSubstring = xElementString.Substring(xElementString.LastIndexOf(":") + 1);
+
+                                Console.WriteLine("Buldum: " + xElementStringToSubstring);
+
+                                choosenNode = xElementString;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("--------");
+
+            foreach (XElement xElementLive in xDocumentLive.Root.Descendants().Where(n => n.Name == wsdl + "types"))
+            {
+                foreach (XElement xElement1 in xElementLive.Descendants(xs + "complexType"))
+                {
+
+                    string xElementToString = xElement1.FirstAttribute.ToString();
+
+                    if(xElementToString.Contains("minOccurs"))
+                    {
+                        string xElementToSubstring = xElementToString.Substring(xElementToString.LastIndexOf("=") + 1);
+
+                        string xElementToSubstringDouble = xElementToSubstring.Substring(1, xElementToSubstring.Length - 2);
+
+                        Console.WriteLine(xElementToSubstringDouble);
+
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                    
+
+
+                  
+                   
+                        
+
+                        
+
+                        
+                    }
+                }
+            }
+
+
+
+
+        }
 }
+
